55f8bd75add69de2489be220416275e9334e3833
Franz <franz0905@web.de>
2020-12-17 22:44:10 +0100

    added Footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-footer color="primary lighten-1" padless>
+    <v-row justify="center" no-gutters>
+      <v-btn v-for="link in links" :key="link" color="white" text class="my-2">
+        {{ link }}
+      </v-btn>
+      <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+        {{ new Date().getFullYear() }} — <strong>NyX</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        'Home',
+        'About Us',
+        'Team',
+      ],
+    };
+  },
+};
+</script>
