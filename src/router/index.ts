fa8ea72f67267e098ff0ca6751542146e6bc2062
Franz <franz0905@web.de>
2020-12-23 21:43:41 +0100

    added /login Route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
 
 Vue.use(VueRouter);
 
@@ -13,7 +14,7 @@ const routes: Array<RouteConfig> = [
   {
     path: '/login',
     name: 'Login',
-    component: Home,
+    component: Login,
   },
   {
     path: '/about',
