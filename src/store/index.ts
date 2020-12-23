f0abd805ff285b3688a9b1219d356d0e0601d1f8
Franz <franz0905@web.de>
2020-12-23 21:44:27 +0100

    added VueX Store for LoginToken

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,35 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import Axios from 'axios';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 
+const getDefaultState = () => ({
+  token: '',
+});
+
 export default new Vuex.Store({
-  state: {
-  },
+  strict: true,
+  plugins: [createPersistedState()],
+  state: getDefaultState(),
   mutations: {
+    SET_TOKEN: (state, token) => {
+      state.token = token;
+    },
+    RESET: (state) => {
+      Object.assign(state, getDefaultState());
+    },
   },
   actions: {
+    login: ({ commit, dispatch }, token) => {
+      commit('SET_TOKEN', token);
+      // set auth header
+      Axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    },
+    logout: ({ commit }) => {
+      commit('RESET', '');
+    },
   },
   modules: {
   },
