506891aca1eddcf4bea81bc7f4ada11189b5c1f9
Jakob Mueller <jakob.mueller@ibot3.de>
2020-12-26 16:12:10 +0100

    Make vue vjsf ready

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,28 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vuetify from 'vuetify';
+import {
+  VTooltip,
+  VIcon,
+  VListItem,
+  VAvatar,
+  VFlex,
+  VLayout,
+  VSelect,
+  VTextarea,
+  VCheckbox,
+} from 'vuetify/lib';
 
 Vue.use(Vuetify);
+
+Vue.component('v-tooltip', VTooltip);
+Vue.component('v-icon', VIcon);
+Vue.component('v-list-item', VListItem);
+Vue.component('v-avatar', VAvatar);
+Vue.component('v-flex', VFlex);
+Vue.component('v-layout', VLayout);
+Vue.component('v-select', VSelect);
+Vue.component('v-textarea', VTextarea);
+Vue.component('v-checkbox', VCheckbox);
 
 export default new Vuetify({
   theme: {
