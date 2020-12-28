1671596d606c27f0e48b621c9a5dd522996b9997
Franz <franz0905@web.de>
2020-12-28 17:11:38 +0100

    now showing locale in url

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,45 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
+import i18n from '@/plugins/i18n';
+import RouterView from '@/views/RouterView.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue'),
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/:locale',
+    component: RouterView,
+    beforeEnter: (to, from, next) => {
+      const { locale } = to.params;
+      if (!i18n.availableLocales.includes(locale)) {
+        next(i18n.locale);
+      }
+      if (i18n.locale !== locale) {
+        i18n.locale = locale;
+      }
+      return next();
+    },
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () => import('../views/Home.vue'),
+      },
+      {
+        path: 'about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue'),
+      },
+    ],
   },
   {
     path: '*',
-    name: 'Page not found',
-    component: Home,
+    redirect() {
+      return i18n.locale;
+    },
   },
 ];
 
