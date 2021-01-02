410dea810383cd6c61014d01a3a66e92facd7a79
Franz <franz0905@web.de>
2021-01-02 14:54:40 +0100

    remove url localization

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,50 +1,39 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import i18n from '@/plugins/i18n';
 import store from '@/store/index';
-import RouterView from '@/views/RouterView.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/:locale',
-    component: RouterView,
-    beforeEnter: (to, from, next) => {
-      const { locale } = to.params;
-      if (!i18n.availableLocales.includes(locale)) {
-        next(`${i18n.locale}${to.fullPath}`);
-      } else {
-        i18n.locale = locale;
-      }
-      return next();
-    },
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        component: () => import('../views/Home.vue'),
-      },
-      {
-        path: 'dashboard',
-        name: 'Dashboard',
-        component: () => import('../views/Dashboard.vue'),
-        meta: { requiresAuth: true },
-      },
-      {
-        path: 'about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue'),
-      },
-    ],
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/Home.vue'),
+  },
+  {
+    path: 'dashboard',
+    name: 'Dashboard',
+    component: () => import('../views/Dashboard.vue'),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: 'about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue'),
+  },
+  {
+    path: 'settings',
+    name: 'Settings',
+    component: () => import('@/views/Settings.vue'),
   },
   {
     path: '*',
+    name: '404 Path not found',
     redirect() {
-      return i18n.locale;
+      return '/';
     },
   },
 ];
