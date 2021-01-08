84ecaf759373aa27dfeb9c0805986a1c8eb1c426
Franz Rodestock <franz0905@web.de>
2021-01-08 19:28:41 +0100

    added tabs component

---
+++ src/components/TabComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-tabs>
+    <v-tab v-for="(tab, index) in tabs" :key="index">
+      <v-icon left>{{ tab.icon }}</v-icon>
+      <span>{{ tab.title }}</span>
+    </v-tab>
+  </v-tabs>
+</template>
+
+<script>
+export default {
+  name: 'TabComponent',
+  props: {
+    tabs: Array,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <PageTitle :title="$t('dashboard.labels.title', { usr: user.username })"/>
+    <TabComponent :tabs="tabs"/>
   </div>
 </template>
 
 <script>
 import PageTitle from '@/components/PageTitle.vue';
+import TabComponent from '@/components/TabComponent.vue';
 
 export default {
   components: {
     PageTitle,
+    TabComponent,
   },
   data() {
     return {
@@ -17,6 +20,17 @@ export default {
       user: {
         username: 'Nutzer 1',
       },
+      tabs: [
+        {
+          title: 'General', icon: 'mdi-adjust',
+        },
+        {
+          title: 'Bundle1', icon: 'mdi-alpha-m-circle',
+        },
+        {
+          title: 'Bundle2', icon: 'mdi-alpha-g-circle',
+        },
+      ],
     };
   },
 };
