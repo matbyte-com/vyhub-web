3dcf0fed5dfaca45f02baebe5f517c53d4fd7690
Franz Rodestock <franz0905@web.de>
2021-01-15 22:18:56 +0100

    use Data Table instead of SimpleTable

--- src/components/DashboardComponents/Groups.vue
+++ src/components/DashboardComponents/Groups.vue
@@ -1,131 +1,66 @@
 <template>
   <div>
-    <h1>Groups</h1>
-    <div class="pb-4">
-      <!--
-      <v-simple-table>
-          <thead>
-            <tr>
-              <th class="text-left">
-                Serverbundle
-              </th>
-              <th class="text-left">
-                Gruppen
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                TTT
-              </td>
-              <td>
-                <span v-for="(group, index) in groups" :key="index">
-                  <v-chip
-                    @mouseover="setActiveProps(group)"
-                    @mouseleave="resetActives"
-                    class="ml-1 mb-1 a secondary"
-                    :class="checkGroups(group)"
-                  >{{ group.name }}
-                  </v-chip>
-                </span>
-              </td>
-            </tr>
-          </tbody>
-      </v-simple-table> -->
-      <v-simple-table v-if="groups.length != 0">
-        <thead>
-          <tr>
-            <th class="text-left">
-              Serverbundle
-            </th>
-            <th class="text-left">
-              Gruppen
-            </th>
-            <th class="text-left">
-              Properties
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="bundle in serverBundles" :key="bundle.id">
-            <td>
-              {{ bundle.name }}
-            </td>
-            <td>
-              <span v-for="group in getGroupsByBundle(bundle.id)" :key="group.id">
-                  <v-chip
-                    @mouseover="setActiveProps(group)"
-                    @mouseleave="resetActives"
-                    class="ml-1 mb-1 a secondary"
-                    :class="checkGroups(group)"
-                  >{{ group.name }}
-                  </v-chip>
-                </span>
-            </td>
-            <td>
-              <span v-for="prop in getPropsByBundle(bundle.id)" :key="prop.id">
+    <h2 class="text-h4">Groups</h2>
+    <v-divider class="mb-3"/>
+    <v-simple-table v-if="groups.length != 0">
+      <thead>
+        <tr>
+          <th class="text-left">
+            Serverbundle
+          </th>
+          <th class="text-left">
+            Gruppen
+          </th>
+          <th class="text-left">
+            Properties
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="bundle in serverBundles" :key="bundle.id">
+          <td>
+            {{ bundle.name }}
+          </td>
+          <td>
+            <span v-for="group in getGroupsByBundle(bundle.id)" :key="group.id">
                 <v-chip
-                  @mouseover="setActiveGroups(prop)"
+                  @mouseover="setActiveProps(group)"
                   @mouseleave="resetActives"
                   class="ml-1 mb-1 a secondary"
-                  :class="checkProps(prop)"
-                >{{ prop.name }}
+                  :class="checkGroups(group)"
+                >{{ group.name }}
                 </v-chip>
               </span>
-            </td>
-          </tr>
-        </tbody>
-      </v-simple-table>
-      <v-expansion-panels flat>
-        <v-expansion-panel class="mt-3">
-          <v-expansion-panel-header class="text-left">Alle Gruppen</v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <v-simple-table>
-              <thead>
-                <tr>
-                  <th>Gruppe</th>
-                  <th>Serverbundle</th>
-                  <th>Startdatum</th>
-                  <th>Enddatum</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="membership in memberships"
-                    :key="membership.id">
-                  <td>
-                    {{ getGroupById(membership.group_id).name }}
-                  </td>
-                  <td>
-                    {{ getBundleById(getGroupById(membership.group_id).serverbundle_id).name }}
-                  </td>
-                  <td>
-                    {{ $d(new Date(membership.begin), 'short', $i18n.locale) }}
-                  </td>
-                  <td>
-                    {{ $d(new Date(membership.end), 'short', $i18n.locale) }}
-                  </td>
-                </tr>
-              </tbody>
-            </v-simple-table>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
-    </div>
-    <!-- <v-divider />
-    <h3>Resultierende Properties:</h3>
-    <div class="pb-4">
-      <span v-for="(prop, index) in getProps" :key="index" >
-        <v-chip
-          @mouseover="setActiveGroups(prop)"
-          @mouseleave="resetActives"
-          small
-          class="ml-1 mb-1 a secondary"
-          :class="checkProps(prop)"
-        >{{ prop.name }}
-        </v-chip>
-      </span>
-    </div> -->
+          </td>
+          <td>
+            <span v-for="prop in getPropsByBundle(bundle.id)" :key="prop.id">
+              <v-chip
+                @mouseover="setActiveGroups(prop)"
+                @mouseleave="resetActives"
+                class="ml-1 mb-1 a secondary"
+                :class="checkProps(prop)"
+              >{{ prop.name }}
+              </v-chip>
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </v-simple-table>
+    <v-expansion-panels flat>
+      <v-expansion-panel class="mt-3">
+        <v-expansion-panel-header class="text-left">Alle Gruppen</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <v-data-table
+          :headers="groupTableHeaders"
+          :items="getMembershipTable"
+          :items-per-page="5"
+          dense
+          >
+            <v-data-footer disable-items-per-page="true" />
+          </v-data-table>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
   </div>
 </template>
 
@@ -143,20 +78,16 @@ export default {
       activeProps: [],
       activeGroups: [],
       test: {},
-      groups: [
-        {
-          name: 'admin', props: ['settings', 'dashboard', 'setting2'],
-        },
-        {
-          name: 'user', props: ['123', '32', '45'],
-        },
-        {
-          name: '123', props: ['12345', 'sdf', '45'],
-        },
-      ],
+      groups: [],
       memberships: [],
       serverBundles: [],
       bundleGroups: [],
+      groupTableHeaders: [
+        { text: 'Name', align: 'start', value: 'name' },
+        { text: 'Serverbundle', value: 'bundle' },
+        { text: 'Startdatum', value: 'startDate' },
+        { text: 'Enddatum', value: 'endDate' },
+      ],
     };
   },
   async beforeMount() {
@@ -173,18 +104,10 @@ export default {
       this.activeGroups = [];
     },
     setActiveProps(group) {
-      // group.props.forEach((prop) => this.activeProps.push(prop));
-      // this.activeGroups.push(group.name);
       Object.values(group.properties).forEach((prop) => this.activeProps.push(prop.name));
-      // group.properties.forEach((prop) => this.activeProps.push(prop.id));
       this.activeGroups.push(group.id);
     },
     setActiveGroups(prop) {
-      // this.groups.forEach((group) => {
-      //  if (group.props.includes(prop)) {
-      //    this.activeGroups.push(group.name);
-      //  }
-      // });
       this.activeGroups.push(prop.group_id);
       this.activeProps.push(prop.name);
     },
@@ -235,6 +158,18 @@ export default {
         Object.values(group.properties).forEach((prop) => props.push(prop));
       });
       return props;
+    },
+    getMembershipTable() {
+      const memberships = [];
+      this.memberships.forEach((m) => {
+        const object = {};
+        object.name = this.getGroupById(m.group_id).name;
+        object.bundle = this.getBundleById(this.getGroupById(m.group_id).serverbundle_id).name;
+        object.startDate = this.$d(new Date(m.begin), 'short', this.$i18n.locale);
+        object.endDate = this.$d(new Date(m.end), 'short', this.$i18n.locale);
+        memberships.push(object);
+      });
+      return memberships;
     },
   },
 };
