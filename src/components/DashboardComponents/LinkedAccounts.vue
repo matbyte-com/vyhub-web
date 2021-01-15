e95d8adca954e9c9ca11d80b4093cdb81b6cf9be
Franz Rodestock <franz0905@web.de>
2021-01-15 23:51:13 +0100

    linked accounts 0.9

--- src/components/DashboardComponents/LinkedAccounts.vue
+++ src/components/DashboardComponents/LinkedAccounts.vue
@@ -1,45 +1,62 @@
 <template>
   <div>
-    <h1>Linked Accounts</h1>
-    <v-simple-table>
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th v-for="(attribute ,index) in linkedAcc[0]" :key="index" class="text-left">
-              {{ index }}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="(acc, index) in linkedAcc"
-            :key="index"
-          >
-            <td>{{ acc.acc }}</td>
-            <td>{{ acc.username }}</td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
+    <h2 class="text-h4">{{ $t("dashboard.labels.linkedAccounts") }}</h2>
+    <v-divider class="mb-3"/>
+    <v-data-table
+    :headers="tableHeader"
+    :items="getAccounts"
+    :hide-default-footer="true">
+    </v-data-table>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import store from '@/store/index';
+
+const uuid = store.getters.user.id;
+const customerURL = `${process.env.VUE_APP_BACKEND_CUSTOMER_URL}`;
+
 export default {
   name: 'LinkedAccounts',
   data() {
     return {
-      linkedAcc: [
-        {
-          acc: 'Central',
-          username: 'Nutzer1',
-        },
-        {
-          acc: 'Gmod',
-          username: 'iBot3',
-        },
+      tableHeader: [
+        { text: this.$t('type'), value: 'type' },
+        { text: this.$t('username'), value: 'username' },
       ],
+      userAccounts: {
+        type: 1,
+        attributes: { username: '' },
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        linked_users: [
+          {
+            type: 1,
+            attributes: { username: '' },
+          },
+        ],
+      },
     };
+  },
+  async beforeMount() {
+    axios.get(`${customerURL}/user/${uuid}`).then((response) => { (this.userAccounts = response.data); });
+  },
+  computed: {
+    getAccounts() {
+      const accs = [];
+      if (this.userAccounts === {}) { return []; }
+      const object = {};
+      object.type = this.userAccounts.type;
+      object.username = this.userAccounts.attributes.username;
+      accs.push(object);
+      this.userAccounts.linked_users.forEach((u) => {
+        const obj = {};
+        obj.type = u.type;
+        obj.username = u.attributes.username;
+        accs.push(obj);
+      });
+      return accs;
+    },
   },
 };
 </script>
