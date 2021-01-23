b5f432e1c1c101a87ad537928c3c219023e9a4aa
Franz Rodestock <franz0905@web.de>
2021-01-23 16:26:18 +0100

    Use of Tabs in the Settings Panel

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,15 +1,60 @@
 <template>
-  <ThemeChanger />
+  <div>
+    <PageTitle :title="$t('settings.labels.title')" />
+    <v-row
+    no-gutters>
+      <v-col
+      cols="2">
+        <v-card flat outlined>
+          <v-tabs @change="switchTab" vertical>
+            <v-tab v-for="tab in tabs" :key="tab.id">
+              <v-icon left>{{ tab.icon }}</v-icon>
+              <span>{{ tab.name }}</span>
+            </v-tab>
+          </v-tabs>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card class="mb-3 pa-3" flat outlined>
+          <keep-alive>
+            <component :is="componentInstance"></component>
+          </keep-alive>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-import { Component, Vue } from 'vue-property-decorator';
-import ThemeChanger from '@/components/SettingComponents/ThemeChanger.vue';
+import PageTitle from '@/components/PageTitle.vue';
 
-@Component({
+export default {
   components: {
-    ThemeChanger,
+    PageTitle,
   },
-})
-export default class Home extends Vue {}
+  data() {
+    return {
+      activeTab: 0,
+      tabs: [
+        {
+          name: 'General', icon: 'mdi-cog', component: '123',
+        },
+        {
+          name: 'Theme', icon: 'mdi-format-color-fill', component: 'ThemeChanger',
+        },
+      ],
+    };
+  },
+  methods: {
+    switchTab(payload) {
+      this.activeTab = this.tabs[payload].component;
+    },
+  },
+  computed: {
+    componentInstance() {
+      const type = this.activeTab;
+      return () => import(`@/components/SettingComponents/${type}`);
+    },
+  },
+};
 </script>
