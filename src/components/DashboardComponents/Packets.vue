99d17579213b214002bdfc982fc68a9a213d72c1
Franz Rodestock <franz0905@web.de>
2021-01-23 14:30:59 +0100

    add the most useful packets.vue

---
+++ src/components/DashboardComponents/Packets.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{ packets }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import store from '@/store';
+
+const uuid = store.getters.user.id;
+const customerURL = `${process.env.VUE_APP_BACKEND_CUSTOMER_URL}`;
+
+export default {
+  name: 'Packets.vue',
+  data() {
+    return {
+      packets: [],
+    };
+  },
+  beforeMount() {
+    axios.get(`${customerURL}/user/${uuid}/packets`).then((response) => { this.userAccounts = response.data; this.componentLoaded = true; });
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
