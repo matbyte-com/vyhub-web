35cd109c9d2b7f9e95643378987bc18cdd3de3cc
Jakob Mueller <jakob.mueller@ibot3.de>
2021-02-14 22:30:20 +0100

    Fix form select fields urls

--- src/forms/Common.ts
+++ src/forms/Common.ts
@@ -2,23 +2,23 @@ const API_URL = process.env.VUE_APP_BACKEND_CUSTOMER_URL;
 
 export default {
   userSelectField: {
-    type: 'string',
+    type: 'object',
     titleK: 'user',
-    'x-fromUrl': `${API_URL}/user?query={q}&max=10`,
+    'x-fromUrl': `${API_URL}/user/?query={q}&max=10`,
     'x-itemKey': 'id',
     'x-itemTitle': 'username',
     'x-itemIcon': 'avatar',
   },
   serverbundleSelectField: {
-    type: 'string',
+    type: 'object',
     titleK: 'serverbundle',
-    'x-fromUrl': `${API_URL}/server/bundle`,
+    'x-fromUrl': `${API_URL}/server/bundle/`,
     'x-itemKey': 'id',
     'x-itemTitle': 'name',
   },
   serverTypeSelectField: {
     type: 'string',
     titleK: 'type',
-    'x-fromUrl': `${API_URL}/server/type`,
+    'x-fromUrl': `${API_URL}/server/type/`,
   },
 };
