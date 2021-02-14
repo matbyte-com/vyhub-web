84052665dfa50adbe3903a644dc2e95fd4b74b05
Franz Rodestock <franz0905@web.de>
2021-02-14 21:04:44 +0100

    Add DeleteConfirmationDialog.vue

---
+++ src/components/DeleteConfirmationDialog.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-dialog v-model="dialog" max-width="500px">
+    <v-card>
+      <v-card-title class="headline">{{ $t('delete') }}</v-card-title>
+      <v-card-text>
+        {{ errorMessage }}
+        <v-row>
+          <v-col cols="12">
+            <v-btn class="mr-4"
+                   depressed
+                   color="primary"
+                   type="submit"
+                   @click="submit">
+              {{ $t('submit') }}
+            </v-btn>
+            <v-btn color="lighten-5" depressed @click="cancel">
+              {{ $t('cancel') }}
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'DeleteConfirmationDialog',
+  data() {
+    return {
+      dialog: null,
+      errorMessage: null,
+      item: null,
+    };
+  },
+  methods: {
+    cancel() {
+      this.dialog = false;
+    },
+    submit() {
+      this.$emit('submitDelete', this.item);
+    },
+    show(item) {
+      this.item = item;
+      this.dialog = true;
+    },
+    setErrorMessage(text) {
+      return this.errorMessage(text);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
