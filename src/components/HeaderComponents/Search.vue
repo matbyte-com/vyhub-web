512774751b07a0a5919cbb231aef8bcf58c5d322
Franz Rodestock <franz0905@web.de>
2021-03-04 22:31:12 +0100

    Fix small bug in the searchbar

--- src/components/HeaderComponents/Search.vue
+++ src/components/HeaderComponents/Search.vue
@@ -1,29 +1,29 @@
 <template>
-  <v-autocomplete
-    @focus="searchClosed = false"
-    @blur="searchClosed = true"
-    v-model="searchInput"
-    :placeholder="$t('user')"
-    prepend-inner-icon="mdi-magnify"
-    class="expanding-search mt-6"
-    :class="{ 'closed' : searchClosed && !search }"
-    filled dense clearable
-    hide-no-data
-    :items="items"
-    :search-input.sync="search"
-    :loading="isLoading"
-    item-text="username"
-    item-value="id"
-    return-object
-    v-on:change="showUser"
-    :filter="searchFilter"
-    @keydown.esc="searchInput = null">
-    <template slot="item"
-              slot-scope="{ item }">
+    <v-autocomplete
+      ref="input"
+      @focus="searchClosed = false"
+      @blur="searchClosed = true, $emit('focusloss')"
+      :placeholder="$t('user')"
+      prepend-inner-icon="mdi-magnify"
+      class="expanding-search mt-6"
+      :class="{ 'closed' : searchClosed && !search }"
+      filled dense clearable
+      hide-no-data
+      :items="items"
+      :search-input.sync="search"
+      :loading="isLoading"
+      item-text="username"
+      item-value="id"
+      return-object
+      v-on:change="showUser"
+      :filter="searchFilter"
+      @keydown.esc="search = null">
+      <template slot="item"
+                slot-scope="{ item }">
         <v-list tile>
           <v-list-item>
             <v-list-item-avatar>
-                <img :src="item.avatar" alt="avatar">
+              <img :src="item.avatar" alt="avatar">
             </v-list-item-avatar>
             <v-list-item-content>
               <v-list-item-title><h2>{{ item.username }}</h2></v-list-item-title>
@@ -43,8 +43,8 @@
             </v-list-item-content>
           </v-list-item>
         </v-list>
-    </template>
-  </v-autocomplete>
+      </template>
+    </v-autocomplete>
 </template>
 
 <script>
@@ -55,7 +55,6 @@ export default {
   name: 'Search',
   data() {
     return {
-      searchInput: null,
       searchClosed: true,
       items: [],
       isLoading: false,
@@ -67,7 +66,13 @@ export default {
     search(query) {
       // Items have already been requested
       if (this.isLoading) return;
-
+      // search CLosed return
+      if (this.searchClosed) return;
+      // return for empty query
+      if (query === '') {
+        this.items = [];
+        return;
+      }
       this.isLoading = true;
 
       // Lazily load input items
@@ -83,11 +88,17 @@ export default {
   methods: {
     showUser(user) {
       if (user != null) {
+        this.searchClosed = true;
+        this.items = [];
         this.$router.push({ name: 'UserDashboard', params: { id: user.id } });
       }
     },
     searchFilter() {
       return true;
+    },
+    test() {
+      console.log(this.searchClosed);
+      console.log(this.searchClosed && !this.search);
     },
   },
 };
