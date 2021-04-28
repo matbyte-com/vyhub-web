615dcf225564b5b814965d4ba3142fab8762147c
Jakob Mueller <jakob.mueller@ibot3.de>
2021-04-28 16:35:48 +0200

    Check availability of axios before usage

--- src/api/openapiCached.ts
+++ src/api/openapiCached.ts
@@ -6,7 +6,10 @@ import axios from 'axios';
 const API_URL = process.env.VUE_APP_BACKEND_CUSTOMER_URL;
 
 async function api() {
-  // TODO error but working!?
+  if (axios.defaults.adapter == null) {
+    throw new Error('axios not available.');
+  }
+
   return new OpenAPIClientAxios({
     definition: `${API_URL}/openapi.json`,
     withServer: 'main',
