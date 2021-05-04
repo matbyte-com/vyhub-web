48404e85900d57c12876c0a0109f4a7e2e78252f
Franz Rodestock <franz0905@web.de>
2021-05-04 20:42:11 +0200

    Remove comment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,5 @@
 <template>
   <div>
-    <!--<v-parallax
-      dark
-      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
-      <v-row align="center" justify="center">
-        <v-col class="text-center" cols="12">
-          <h1 class="display-1 font-weight-thin mb-4">
-            G Extension
-          </h1>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-spacer></v-spacer>
-        <v-col
-        class="text-center justify-center"
-        cols="4">
-          <p>
-            Herzlich willkommen auf unserem Server.
-            Für Fragen wende dich an uns über das Kontaktformular oder über Teamspeak
-          </p>
-        </v-col>
-        <v-spacer></v-spacer>
-      </v-row>
-      <v-overlay
-        :absolute="true"
-        :value="true"
-        color="primary"
-      :opacity="0.3">
-      </v-overlay>
-    </v-parallax>
-    <v-divider />-->
     <!-- Add Message Dialo -->
     <dialog-form ref="messageAddDialog" :form-schema="messageAddSchema"
                  :title="$t('home.addNews')"
