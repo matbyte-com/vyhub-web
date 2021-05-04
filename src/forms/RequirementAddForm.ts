31c75979725f0a7e1047e9753668ad03a6b51052
Franz Rodestock <franz0905@web.de>
2021-05-04 20:42:56 +0200

    Add RequirementAddForms

---
+++ src/forms/RequirementAddForm.ts
@@ -0,0 +1,74 @@
+import i18n from '@/plugins/i18n';
+import openapiCached from '@/api/openapiCached';
+import VueI18n from 'vue-i18n';
+
+// const API_URL = process.env.VUE_APP_BACKEND_CUSTOMER_URL;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+enum RequirementOperator {
+  EQ = 'EQ',
+  LEQ = 'LEQ',
+  GEQ = 'GEQ',
+  LT = 'LT',
+  GT = 'GT',
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE',
+  NEVER_ACTIVE = 'NEVER_ACTIVE',
+  HAVE = 'HAVE',
+}
+
+const typeMapping = {
+  ACTIVE: ['ACTIVE', 'INACTIVE', 'NEVER_ACTIVE'],
+  NUMBER: ['EQ', 'LEQ', 'GEQ', 'LT', 'GT'],
+  HAVE: ['HAVE'],
+};
+
+const types = {
+  GROUP_MEMBER: ['mdi-account-multiple', typeMapping.ACTIVE],
+  PERMISSION_LEVEL: ['mdi-numeric-9-plus-box-outline', typeMapping.NUMBER],
+  PERMISSION_LEVEL_SB: ['mdi-numeric-9-plus-box-multiple-outline', typeMapping.NUMBER],
+  PROPERTY: ['mdi-asterisk', typeMapping.HAVE],
+  PROPERTY_SB: ['mdi-atom', typeMapping.HAVE],
+  USER_ATTRIBUTE: ['mdi-account-details', typeMapping.HAVE],
+  PACKET: ['mdi-gift-open', typeMapping.ACTIVE],
+  DATE: ['mdi-calendar', typeMapping.NUMBER],
+  USER_SELF: ['mdi-account-box', typeMapping.HAVE],
+};
+
+function returnForm() {
+  const oneOf: { const: string; title: string }[] = [];
+  Object.entries(types).forEach(([key, value]) => {
+    const reqType = {
+      const: '',
+      title: '',
+      icon: '',
+    };
+    reqType.const = key;
+    reqType.title = i18n.t(`_requirement.types.${key}`).toString();
+    reqType.icon = value[0].toString();
+    oneOf.push(reqType);
+  });
+  const form = {
+    type: 'object',
+    required: [
+      'type',
+    ],
+    properties: {
+      type: {
+        type: 'string',
+        title: i18n.t('_requirement.requirement_type'),
+        'x-display': 'icon',
+        'x-itemIcon': 'icon',
+        default: 'GROUP_MEMBER',
+        oneOf,
+      },
+    },
+  };
+  return form;
+}
+
+export default {
+  returnForm,
+  types,
+  typeMapping,
+};
---
+++ src/forms/RequirementAddFormConditional.ts
@@ -0,0 +1,130 @@
+import i18n from '@/plugins/i18n';
+import RequirementAddForm from './RequirementAddForm';
+import Common from './Common';
+import Properties from '../services/PropertyService';
+
+const API_URL = process.env.VUE_APP_BACKEND_CUSTOMER_URL;
+
+const operatorMapping = {
+  EQ: 'mdi-equal',
+  LEQ: 'mdi-less-than-or-equal',
+  GEQ: 'mdi-greater-than-or-equal',
+  LT: 'mdi-less-than',
+  GT: 'mdi-greater-than',
+  ACTIVE: 'mdi-account-check',
+  INACTIVE: 'mdi-account-alert',
+  NEVER_ACTIVE: 'mdi-account-cancel',
+  HAVE: 'mdi-account-plus',
+};
+
+function returnInput(type: string) {
+  // TODO type.USER_ATTRIBUTE, type.USER_SELF missing
+  if (type === 'PERMISSION_LEVEL' || type === 'PERMISSION_LEVEL_SB') {
+    return {
+      input: {
+        type: 'number',
+        title: i18n.t('_requirement.permissionLevel'),
+        minimum: 0,
+        maximum: 100,
+      },
+    };
+  }
+  // TODO Make use of proper serverbundle group picker
+  if (type === 'GROUP_MEMBER') {
+    return {
+      input: {
+        type: 'object',
+        title: i18n.t('group'),
+        // 'x-fromUrl': `${API_URL}/group/?serverbundle_id={serverbundle.id}`,
+        'x-fromUrl': `${API_URL}/group/`,
+        'x-itemKey': 'id',
+        'x-itemTitle': 'name',
+        'x-display': 'icon',
+        'x-itemIcon': 'icon',
+      },
+    };
+  }
+  if (type === 'PROPERTY' || type === 'PROPERTY_SB') {
+    const oneOf: { const: string; title: string }[] = [];
+    Object.entries(Properties).forEach(([key, value]) => {
+      const prop = {
+        const: '',
+        title: '',
+      };
+      prop.const = key;
+      prop.title = `${key}: ${i18n.t(`_properties.${key}`)}`;
+      oneOf.push(prop);
+    });
+    return {
+      input: {
+        type: 'string',
+        title: i18n.t('property'),
+        oneOf,
+      },
+    };
+  }
+  if (type === 'DATE') {
+    return {
+      begin: {
+        type: 'string',
+        title: i18n.t('begin'),
+        format: 'date-time',
+        default: new Date(),
+      },
+      end: {
+        type: 'string',
+        title: i18n.t('end'),
+        format: 'date-time',
+        description: i18n.t('_dashboard.membershipEndDescription'),
+      },
+    };
+  }
+  if (type === 'PACKET') {
+    return {
+      input: Common.packetSelectField,
+    };
+  }
+  return null;
+}
+
+function returnForm(type: string) {
+  const oneOf: { const: string; title: string }[] = [];
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  const operators = RequirementAddForm.types[type][1];
+  operators.forEach((op: string) => {
+    const reqType = {
+      const: '',
+      title: '',
+      icon: '',
+    };
+    reqType.const = op;
+    reqType.title = i18n.t(`_requirement.operators.${op}`).toString();
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore
+    reqType.icon = operatorMapping[op];
+    oneOf.push(reqType);
+  });
+  const form = {
+    type: 'object',
+    required: [
+      'type',
+    ],
+    properties: {
+      operator: {
+        type: 'string',
+        title: i18n.t('_requirement.requirement_operator'),
+        'x-display': 'icon',
+        'x-itemIcon': 'icon',
+        default: 'ACTIVE',
+        oneOf,
+      },
+      ...returnInput(type),
+    },
+  };
+  return form;
+}
+
+export default {
+  returnForm,
+};
