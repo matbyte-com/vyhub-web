9986917a076975101b14cd39e5c6807f9f75c32b
Franz Rodestock <franz0905@web.de>
2021-05-13 13:39:36 +0200

    Change Router Scroll Behavior

--- src/router/index.ts
+++ src/router/index.ts
@@ -99,6 +99,12 @@ const routes: Array<RouteConfig> = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { x: 0, y: 0 };
+  },
   routes,
 });
 
