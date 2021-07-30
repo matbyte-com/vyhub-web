8ec3a90e7b40c88dc5e7bfd97a6840636fcc7d0c
ibot3 <ibot3@users.noreply.github.com>
2021-07-30 20:17:39 +0000

    Build: (09f8c31) Move config.js to config.js.example

--- config.js
+++
@@ -1,4 +0,0 @@
-window.vyhubConfig = {
-  backend_url: 'http://nyx.devapp:5050/v1',
-  default_title: 'VyHub',
-};
---
+++ config.js.example
@@ -0,0 +1,4 @@
+window.vyhubConfig = {
+  backend_url: 'http://nyx.devapp:5050/v1',
+  default_title: 'VyHub',
+};
