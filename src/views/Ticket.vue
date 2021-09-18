99759fef98d493c38c84edee3208b4794371599f
Franz <franz0905@web.de>
2021-09-18 23:11:23 +0300

    Improved ticket overview design

--- src/views/Ticket.vue
+++ src/views/Ticket.vue
@@ -2,7 +2,7 @@
   <div>
     <ThreadAddDialog ref="addThreadDialog" :dialog-title="$t('_ticket.addTicket')"
                      @submit="newThread"/>
-    <div class="d-flex">
+    <div class="d-flex mb-5">
       <PageTitle icon="mdi-ticket-confirmation">{{ $t('_ticket.tickets') }}</PageTitle>
       <v-spacer />
       <v-btn color="success" @click="$refs.addThreadDialog.show()">
@@ -10,39 +10,45 @@
         <span>{{ $t('_ticket.addTicket') }}</span>
       </v-btn>
     </div>
-    <v-card v-for="ticket in tickets" :key="ticket.id" class="mb-3"
-            :to="{ name: 'Thread', params: { id: ticket.id } }">
-      <v-card-title>
-        <v-avatar>
-          <v-img :src="ticket.creator.avatar"/>
-        </v-avatar>
-        <span class="ml-3">{{ ticket.title }}</span>
-        <v-spacer />
-        <span class="text--disabled subtitle-2">
-          {{ $d(new Date(ticket.created), 'long') }}
-        </span>
-        <span class="ml-3 mb-1">
-          <v-btn outlined color="primary" small
-                 @click="openEditMessageDialog(message)" class="mr-1">
-            <v-icon>
-              mdi-pencil
-            </v-icon>
-          </v-btn>
-          <v-btn outlined color="error" small @click="openDeleteMessageDialog(message)">
-            <v-icon>
-              mdi-delete
-            </v-icon>
-          </v-btn>
-        </span>
-      </v-card-title>
-      <v-card-text class="ql-editor ml-15">
-        <span v-html="ticket.content.substr(0,300)" />
-        <div class="d-flex align-center" v-if="ticket.content.length > 3">
-          <v-icon>mdi-arrow-right-top</v-icon>
-          <span>... {{ $t('_ticket.readMore') }}</span>
-        </div>
-      </v-card-text>
-    </v-card>
+    <div class="d-flex mt-3" v-for="ticket in tickets" :key="ticket.id">
+      <v-avatar size="100px">
+        <v-img :src="ticket.creator.avatar"/>
+      </v-avatar>
+      <v-card :to="{ name: 'Thread', params: { id: ticket.id } }" class="ml-10" flat outlined
+              width="100%" >
+        <v-card-title :class="{ 'grey-title': !$vuetify.theme.dark }">
+          <v-row>
+            <v-col>
+              {{ ticket.title }}
+            </v-col>
+            <v-col v-if="$checkProp('news_edit')" class="text-right">
+              <v-btn outlined color="primary" small
+                     @click="openEditMessageDialog()" class="mr-1">
+                <v-icon>
+                  mdi-pencil
+                </v-icon>
+              </v-btn>
+              <v-btn outlined color="error" small @click="openDeleteMessageDialog()">
+                <v-icon>
+                  mdi-delete
+                </v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card-title>
+        <v-card-text class="mt-3">
+          <span v-html="ticket.content.substr(0,300)" class="ql-editor"/>
+          <div class="d-flex align-center" v-if="ticket.content.length > 300">
+            <v-icon>mdi-arrow-right-top</v-icon>
+            <span>... {{ $t('_ticket.readMore') }}</span>
+          </div>
+        </v-card-text>
+        <v-card-actions class="text--disabled pt-0">
+          <span class="mr-3">{{ $d(new Date(ticket.created), 'long') }}</span>
+          <user-link v-if="ticket.creator" :user="ticket.creator"/>
+        </v-card-actions>
+      </v-card>
+    </div>
     <v-card v-if="tickets === []">
       {{ $t('_ticket.noTickets') }}
     </v-card>
@@ -53,11 +59,13 @@
 import PageTitle from '../components/PageTitle.vue';
 import openapi from '../api/openapi';
 import ThreadAddDialog from '../components/ForumComponents/ThreadAddDialog.vue';
+import UserLink from '../components/UserLink.vue';
 
 export default {
   components: {
     ThreadAddDialog,
     PageTitle,
+    UserLink,
   },
   name: 'Ticket.vue',
   data() {
