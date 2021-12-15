7d7cf9cfa15002e42115fa5f849b5452fb4b4bf0
Franz Rodestock <franz0905@web.de>
2021-12-15 18:38:26 +0100

    Ability to update emails

--- src/components/PersonalSettings.vue
+++ src/components/PersonalSettings.vue
@@ -1,15 +1,18 @@
 <template>
-  <Dialog ref="dialog" @cancel="$router.push($route.path, query: {} )">
-    Test
+  <Dialog ref="dialog" @cancel="$router.push($route.path)" :title="$t('_personalSettings.title')"
+          @close="$router.push($route.path)"
+          icon="mdi-account">
+    <Email class="mt-3"/>
   </Dialog>
 </template>
 
 <script>
 import Dialog from '@/components/Dialog.vue';
+import Email from '@/components/PersonalSettings/Email.vue';
 
 export default {
   name: 'PersonalSettings',
-  components: { Dialog },
+  components: { Email, Dialog },
   watch: {
     $route(to) {
       if (to.query.personal_settings === 'true') {
---
+++ src/components/PersonalSettings/Email.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <v-icon left>mdi-email</v-icon>
+      {{ $t('_personalSettings.email') }}
+    </v-card-title>
+    <v-card-text class="body-1">
+      <div v-if="!showInput">
+        <div v-if="email != null">
+          {{ email }}
+        </div>
+        <div v-else>{{ $t('_personalSettings.noEmailSpecified') }}</div>
+      </div>
+      <v-text-field v-else v-model="emailModel" hide-details="auto" ref="textfield"
+                    :label="$t('_personalSettings.email')" :rules="[validator]"
+                    @keydown.enter="updateMail"/>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn text color="warning" @click="this.showInputFunc" v-if="!showInput">
+        <v-icon left>mdi-pencil</v-icon>
+        {{ $t('edit') }}
+      </v-btn>
+      <v-btn v-else text color="success" @click="this.updateMail">
+        <v-icon left>mdi-check</v-icon>
+        {{ $t('update') }}
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import openapi from '@/api/openapi';
+
+export default {
+  name: 'Email',
+  data() {
+    return {
+      email: null,
+      showInput: false,
+      emailModel: null,
+      validator: (value) => {
+        const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return pattern.test(value) || 'Invalid e-mail.';
+      },
+    };
+  },
+  beforeMount() {
+    this.getEmail();
+  },
+  methods: {
+    showInputFunc() {
+      this.emailModel = this.email;
+      this.showInput = true;
+      this.$nextTick(() => {
+        this.$refs.textfield.focus();
+      });
+    },
+    async getEmail() {
+      (await openapi).user_getEmail(this.$store.getters.user.id, null).then((rsp) => {
+        if (!rsp.data) { return; }
+        this.email = rsp.data.email;
+      });
+    },
+    async updateMail() {
+      (await openapi).user_updateEmail(this.$store.getters.user.id, { email: this.emailModel })
+        .then((rsp) => {
+          this.showInput = false;
+          this.$notify({
+            title: this.$t('_personalSettings.messages.emailUpdated'),
+            type: 'success',
+          });
+          this.email = rsp.data.email;
+        }).catch((err) => {
+          this.$notify({
+            title: `${this.$t('unexpectedError')} ${err.response.status}`,
+            text: err.response.data.detail,
+            type: 'error',
+          });
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
