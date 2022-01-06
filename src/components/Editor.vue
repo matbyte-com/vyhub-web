cad15ed90923eb0fc87dbe9057b26de467729cb9
Franz Rodestock <franz0905@web.de>
2022-01-06 15:36:07 +0100

    Remove Video from Editor Toolbar

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <vue-editor @ready="ready" v-model="content"></vue-editor>
+    <vue-editor :editor-toolbar="toolbar" @ready="ready" v-model="content"></vue-editor>
   </div>
 </template>
 
@@ -17,15 +17,21 @@ export default {
     return {
       quill: null,
       toolbar: [
+        [{ header: [false, 1, 2, 3, 4, 5, 6] }],
         ['bold', 'italic', 'underline', 'strike'],
+        [
+          { align: '' },
+          { align: 'center' },
+          { align: 'right' },
+          { align: 'justify' },
+        ],
         ['blockquote', 'code-block'],
 
-        [{ color: [] }, { background: [] }],
-        [{ font: [] }],
-        [{ align: [] }],
-
-        ['clean'],
-        ['image'],
+        [{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
+        [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+        [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+        ['link', 'image'],
+        ['clean'], // remove formatting button
       ],
     };
   },
