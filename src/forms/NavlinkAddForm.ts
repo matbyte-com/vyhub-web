1e0d5d4718c6b9b597c9ac765f5c8f999d534c80
Franz Rodestock <franz0905@web.de>
2022-01-07 01:21:05 +0100

    Update navigation link add form

--- src/forms/NavlinkAddForm.ts
+++ src/forms/NavlinkAddForm.ts
@@ -4,38 +4,58 @@ import Common from '@/forms/Common';
 function returnForm(disabled?: false) {
   return {
     type: 'object',
-    required: [
-      'title',
-      'linkType',
-      'icon',
-    ],
-    properties: {
-      title: {
-        type: 'string',
-        title: i18n.t('title'),
-      },
-      enabled: {
-        type: 'boolean',
-        default: false,
-        title: i18n.t('enabled'),
-      },
-      icon: Common.iconPicker,
-      linkType: {
-        type: 'string',
-        title: i18n.t('type'),
-        readOnly: disabled,
-        oneOf: [
-          {
-            const: 'link',
-            title: i18n.t('settings.externalLink'),
-          },
-          {
-            const: 'html',
-            title: i18n.t('settings.htmlContent'),
-          },
+    allOf: [
+      {
+        required: [
+          'title',
+          'linkType',
+          'icon',
         ],
+        properties: {
+          title: {
+            type: 'string',
+            title: i18n.t('_navigation.title'),
+          },
+          enabled: {
+            type: 'boolean',
+            default: false,
+            title: i18n.t('_navigation.enabled'),
+          },
+          icon: Common.iconPicker,
+          linkType: {
+            type: 'string',
+            title: i18n.t('_navigation.type'),
+            readOnly: disabled,
+            oneOf: [
+              {
+                const: 'link',
+                title: i18n.t('_navigation.externalLink'),
+              },
+              {
+                const: 'html',
+                title: i18n.t('_navigation.htmlContent'),
+              },
+            ],
+          },
+        },
+        if: {
+          required: ['linkType'],
+          properties: {
+            linkType: {
+              const: 'link',
+            },
+          },
+        },
+        then: {
+          properties: {
+            link: {
+              type: 'string',
+              title: i18n.t('_navigation.link'),
+            },
+          },
+        },
       },
-    },
+    ],
   };
 }
 
