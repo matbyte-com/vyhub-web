d37708cdc8a4187e8e807d92f5ab7283acc8e539
Jakob Mueller <jakob.mueller@ibot3.de>
2022-12-22 12:27:29 +0100

    Add missing ticket category

--- src/views/Ticket.vue
+++ src/views/Ticket.vue
@@ -94,6 +94,7 @@ export default {
     },
     async newThread() {
       const data = this.$refs.addThreadDialog.getData();
+      data.category = 'TICKET';
       (await openapi).forum_newThread(null, data).then(() => {
         this.$refs.addThreadDialog.close();
         this.fetchData();
