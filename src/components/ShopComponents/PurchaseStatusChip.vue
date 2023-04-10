14d446dd1f07581bb08b21beaef3329c6a564405
Jakob Mueller <jakob.mueller@ibot3.de>
2023-04-10 09:56:07 +0200

    Fix purchase status text color

--- src/components/ShopComponents/PurchaseStatusChip.vue
+++ src/components/ShopComponents/PurchaseStatusChip.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
-      <v-chip v-if="status === 'CANCELLED'">{{ $t('cancelled') }}</v-chip>
-      <v-chip v-if="status === 'OPEN'" color="info">{{ $t('open') }}</v-chip>
-      <v-chip v-if="status === 'FINISHED'" color="green">{{ $t('finished') }}</v-chip>
-      <v-chip v-if="status === 'REVOKED'" color="orange">
+      <v-chip class="white--text" v-if="status === 'CANCELLED'">{{ $t('cancelled') }}</v-chip>
+      <v-chip class="white--text" v-if="status === 'OPEN'" color="info">{{ $t('open') }}</v-chip>
+      <v-chip class="white--text" v-if="status === 'FINISHED'" color="green">{{ $t('finished') }}
+      </v-chip>
+      <v-chip class="white--text" v-if="status === 'REVOKED'" color="orange">
         {{ $t('_purchases.labels.revoked') }}
       </v-chip>
-      <v-chip v-if="status === 'REFUNDED'" color="red">
+      <v-chip class="white--text" v-if="status === 'REFUNDED'" color="red">
         {{ $t('_purchases.labels.refunded') }}
       </v-chip>
-      <v-chip v-if="status === 'RECURRING'" color="purple">{{ $t('recurring') }}</v-chip>
+      <v-chip class="white--text" v-if="status === 'RECURRING'" color="purple">{{ $t('recurring') }}
+      </v-chip>
     </div>
 </template>
 
