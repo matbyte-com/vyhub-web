900af8f2852a7bad087e62efac3fdd449fb178ca
Franz Rodestock <franz0905@web.de>
2023-05-06 12:46:37 +0200

    Add regex validation to ban-protest-url

--- src/forms/BanSettingsForm.ts
+++ src/forms/BanSettingsForm.ts
@@ -11,6 +11,7 @@ export default {
         clearable: true,
       },
       description: i18n.t('_ban.externalProtestUrlDescription'),
+      pattern: 'https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)',
     },
   },
 };
