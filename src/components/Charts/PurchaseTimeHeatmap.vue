56ac057b159b29f1929b0c5f89063862d49c2f17
Franz Rodestock <franz0905@web.de>
2023-05-24 21:39:26 +0200

    Add purchase heatmap

---
+++ src/components/Charts/PurchaseTimeHeatmap.vue
@@ -0,0 +1,131 @@
+<template>
+  <div>
+    <div v-if="series != null">
+      <apexchart type="heatmap" :options="options" :series="series"></apexchart>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PurchaseTimeHeatmap',
+  props: {
+    data: Array,
+  },
+  data() {
+    return {
+      options: {
+        theme: {
+          mode: (this.$vuetify.theme.dark ? 'dark' : 'light'),
+        },
+        colors: ['#008FFB'],
+        chart: {
+          id: 'purchase-heatmap',
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        xaxis: {
+          title: {
+            text: this.$t('time'),
+          },
+          labels: {
+            formatter: (y) => (this.convertTimeRange(y)),
+          },
+        },
+        yaxis: {
+          labels: {
+            formatter: (y) => (this.getLocalizedWeekday(y)),
+          },
+        },
+        tooltip: {
+          custom({
+            series, seriesIndex, dataPointIndex, w,
+          }) {
+            if (series[seriesIndex][dataPointIndex] === 0) {
+              return '';
+            }
+            return `${'<div class="arrow_box pa-2">'
+              + '<span>'}${series[seriesIndex][dataPointIndex]}</span>`
+              + '</div>';
+          },
+        },
+
+      },
+    };
+  },
+  methods: {
+    getLocalizedWeekday(day) {
+      const date = new Date();
+      date.setDate(date.getDate() + day - 2);
+      return date.toLocaleString(this.$i18n.locale, { weekday: 'long' });
+    },
+    convertTimeRange(timeRange) {
+      // do some timezone magic to cope with different timezones to display
+      // the result in the user's timezone
+      const offsetMinutes = new Date().getTimezoneOffset();
+      const offsetHours = -(offsetMinutes / 60);
+      const startHour = ((timeRange * 2) + offsetHours) % 24;
+      const endHour = (startHour + 2) % 24;
+      const startTime = `${startHour.toString().padStart(2, '0')}`;
+      const endTime = `${endHour.toString().padStart(2, '0')}`;
+      return `${startTime} - ${endTime}`;
+    },
+  },
+  computed: {
+    series() {
+      if (this.data == null) {
+        return null;
+      }
+
+      // Initialize the series array
+      const series = [];
+
+      this.data.forEach((item) => {
+        const { day, time_interval, purchase_count } = item;
+
+        // eslint-disable-next-line no-prototype-builtins
+        if (!series.hasOwnProperty(day)) {
+          series[day] = {};
+        }
+
+        series[day][time_interval] = purchase_count;
+      });
+
+      // Add missing values for days and purchase counts
+      for (let day = 1; day <= 7; day += 1) {
+        // eslint-disable-next-line no-prototype-builtins
+        if (!series.hasOwnProperty(day)) {
+          series[day] = {};
+        }
+
+        for (let interval = 0; interval <= 11; interval += 1) {
+          // eslint-disable-next-line no-prototype-builtins
+          if (!series[day].hasOwnProperty(interval)) {
+            series[day][interval] = 0;
+          }
+        }
+      }
+
+      // Convert the series object into the required ApexCharts format
+      const apexSeries = Object.entries(series).map(([day, intervals]) => {
+        const timeSeries = Object.entries(intervals).map(([interval, count]) => ({
+          x: interval,
+          y: count,
+        }));
+
+        return {
+          name: day,
+          data: timeSeries,
+        };
+      });
+
+      return apexSeries.reverse();
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
