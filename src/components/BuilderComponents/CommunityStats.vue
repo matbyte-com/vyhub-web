a70fd12f0c4269d40b004cb141b904214c7cff6b
Franz Rodestock <franz0905@web.de>
2023-08-02 15:16:52 +0200

    Add community stats

---
+++ src/components/BuilderComponents/CommunityStats.vue
@@ -0,0 +1,125 @@
+<template>
+  <v-card>
+    <v-card-text class="pa-5">
+      <div class="text-center text-h3 pb-5">
+        {{ headline }}
+      </div>
+      <v-row class="justify-center">
+        <v-col>
+          <div class="d-flex flex-row flex-wrap">
+            <v-card v-for="stat in stats" :key="stat.title" class="ml-3 stat-card" flat>
+              <v-card-text class="d-flex align-center">
+                <div>
+                  <v-icon class="number" size="50px">{{ stat.icon }}</v-icon>
+                </div>
+                <div class="ml-3">
+                  <div class="text-h5">{{ stat.title }}</div>
+                  <div class="text-h4 text-center number">{{ currentCount[stat.key] }}</div>
+                </div>
+              </v-card-text>
+            </v-card>
+          </div>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import openapi from '@/api/openapi';
+
+export default {
+  name: 'CommunityStats',
+  props: {
+    headline: {
+      type: String,
+      default: 'Community',
+    },
+  },
+  data() {
+    return {
+      stats: [
+        {
+          title: 'Online Players',
+          key: 'online_players',
+          icon: 'mdi-gamepad-right',
+        },
+        {
+          title: 'Active Spieler',
+          key: 'active_users',
+          icon: 'mdi-account-clock-outline',
+        },
+        {
+          title: 'Registered Players',
+          key: 'registered_players',
+          icon: 'mdi-account-group',
+        },
+        {
+          title: 'Hours Played',
+          key: 'hours_played',
+          icon: 'mdi-clock-fast',
+        },
+      ],
+      currentCount: {
+        online_players: 0,
+        registered_players: 0,
+        hours_played: 0,
+        active_users: 0,
+      },
+      finalCount: {
+        online_players: 0,
+        registered_players: 0,
+        hours_played: 0,
+        active_users: 0,
+      },
+      tick_amount: {
+        online_players: 2,
+        registered_players: 2,
+        hours_played: 2,
+        active_users: 2,
+      },
+    };
+  },
+  beforeMount() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      (await openapi).general_getCommunityStats().then((rsp) => {
+        this.finalCount = rsp.data;
+        this.calculateSpeed();
+        Object.keys(this.finalCount).forEach((key, value) => {
+          this.count(key, 0);
+        });
+      });
+    },
+    calculateSpeed() {
+      Object.keys(this.finalCount).forEach((key, value) => {
+        // Reduce the divider to make the speed go faster
+        let speed = Math.floor(this.finalCount[key] / (7 * 10));
+        if (speed <= 1) speed = 1;
+        this.tick_amount[key] = speed;
+      });
+    },
+    count(type, number) {
+      this.currentCount[type] = number;
+      if (number >= this.finalCount[type]) {
+        return;
+      }
+      setTimeout(() => {
+        this.count(type, number + this.tick_amount[type]);
+      }, 10);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.number {
+  transition: all .2s ease-in-out;
+}
+
+.stat-card:hover .number {
+  color: var(--v-primary-base);
+}
+</style>
---
+++ src/components/BuilderComponents/components.ts
@@ -0,0 +1,88 @@
+export default {
+  components: [
+    {
+      title: 'Headline',
+      component: 'Headline',
+      props: [
+        {
+          name: 'title',
+          type: 'string',
+        },
+        {
+          name: 'subtitle',
+          type: 'string',
+        },
+        {
+          name: 'height',
+          type: 'string',
+        },
+        {
+          name: 'imageUrl',
+          type: 'string',
+        },
+      ],
+    },
+    {
+      title: 'Carousel',
+      component: 'Carousel',
+      props: [
+        {
+          name: 'slides',
+          type: 'array',
+        },
+        {
+          name: 'height',
+          type: 'string',
+        },
+      ],
+    },
+    {
+      title: 'HTML',
+      component: 'RawHtml',
+      props: [
+        {
+          name: 'html',
+          type: 'string',
+        },
+      ],
+    },
+    {
+      title: 'New Users',
+      component: 'NewUsers',
+      props: [
+        {
+          name: 'limit',
+          type: 'number',
+        },
+      ],
+    },
+    {
+      title: 'Discord',
+      component: 'Discord',
+      props: [
+        {
+          name: 'headline',
+          type: 'string',
+        },
+        {
+          name: 'discordWidget',
+          type: 'string',
+        },
+        {
+          name: 'mirror',
+          type: 'boolean',
+        },
+      ],
+    },
+    {
+      title: 'Community Stats',
+      component: 'CommunityStats',
+      props: [
+        {
+          name: 'headline',
+          type: 'string',
+        },
+      ],
+    },
+  ],
+};
