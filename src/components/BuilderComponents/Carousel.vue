1522b5d9268b81bafd25d83b8c285d32f1155a63
Franz Rodestock <franz0905@web.de>
2023-08-02 11:29:42 +0200

    Add Carousel Block

---
+++ src/components/BuilderComponents/Carousel.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-carousel hide-delimiters :height="height ? height : '500px'">
+      <v-carousel-item v-for="slide in slides" :key="slide.title + slide.imageUrl"
+                       :src="slide.imageUrl">
+        <div style="width: 100%; height: 100%"
+             class="d-flex flex-column justify-center align-center text-center">
+          <h1 class="text-h3">{{ slide.title }}</h1>
+          <p class="text-subtitle-1 mt-3">{{ slide.subtitle }}</p>
+        </div>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Carousel',
+  props: ['titles', 'subtitles', 'height', 'imageUrls', 'slides'],
+};
+</script>
+
+<style scoped>
+
+</style>
