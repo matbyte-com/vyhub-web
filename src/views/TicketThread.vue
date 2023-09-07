5d327dcd6b15a034fa88bdc480fc8adf37155b18
Franz Rodestock <franz0905@web.de>
2023-09-07 15:12:43 +0200

    Improve Ticket

--- src/views/TicketThread.vue
+++ src/views/TicketThread.vue
@@ -3,37 +3,15 @@
     <ThreadAddDialog ref="addPostDialog"
                      :dialog-title="`${$t('_forum.addPost')}`"
                      @submit="newPost" :hide-title-input="true"/>
-    <!--
-    <PageTitle v-if="thread && posts"
-               icon="mdi-forum"
-               class="mb-5"
-               show-subtitle
-               :title="thread.title">
-      <template v-slot:subtitle>
-        <v-row>
-          <v-col cols="12" sm="9" align-self="center" style="white-space: nowrap">
-            <user-link simple class="ml-1" v-if="thread.creator" :user="thread.creator"/>
-            {{ utils.formatDate(thread.created) }}
-          </v-col>
-          <v-col cols="12" sm="3">
-            <v-chip v-if="thread.status === 'OPEN'" color="success" class="text-uppercase"
-                    @click="toggleStatus()">
-              {{ $t('_forum.open') }}
-            </v-chip>
-            <v-chip v-else color="error" class="text-uppercase" @click="toggleStatus()">
-              {{ $t('_forum.closed') }}
-            </v-chip>
-          </v-col>
-        </v-row>
-      </template>
-    </PageTitle>
-    -->
-    <PageTitleFlat :title="thread.title" class="mb-5"
+    <PageTitleFlat :title="thread ? thread.title : ''"
+                   :class="{ 'mb-4':!$vuetify.breakpoint.smAndDown}"
+                   :no-bottom-border-radius="$vuetify.breakpoint.smAndDown"
                    :hide-triangle="$vuetify.breakpoint.smAndDown"
                    :open="$vuetify.breakpoint.smAndDown">
       <template v-slot:end>
         <div class="d-flex flex-column">
-          <v-chip v-if="thread.status === 'OPEN'" color="success"  class="text-uppercase ml-auto">
+          <v-chip v-if="thread && thread.status === 'OPEN'"
+                  color="success"  class="text-uppercase ml-auto">
             {{ $t('_forum.open') }}
           </v-chip>
           <v-chip v-else color="error" class="text-uppercase ml-auto">
@@ -42,102 +20,93 @@
         </div>
       </template>
     </PageTitleFlat>
-    <div v-if="!posts" class="mr-15 ml-15">
-      <v-skeleton-loader type="article"/>
-      <v-skeleton-loader class="mt-3" type="article"/>
-    </div>
-    <div class="mt-3" v-if="thread">
-      <div class="mt-3" v-for="post in posts" :key="post.id">
-        <v-card flat outlined class="vh-forum-post card-rounded">
-          <div class="d-flex" :class="{ 'flex-column' : $vuetify.breakpoint.xs }">
-            <div class="pa-3 text-center" style="width: 150px" v-if="$vuetify.breakpoint.smAndUp">
-              <router-link :to="{ name: 'UserDashboard', params: {id: post.creator.id}}"
-                           class="stylelint" style="color: inherit">
-                <v-avatar size="80">
-                  <v-img class="mx-auto" :src="post.creator.avatar" />
-                </v-avatar>
-                <div class="text-h6">
-                  {{ post.creator.username }}
-                </div>
-              </router-link>
-            </div>
-            <div v-else class="pa-3">
-              <router-link :to="{ name: 'UserDashboard', params: {id: post.creator.id}}"
-                           class="stylelint d-flex justify-center align-center"
-                           style="color: inherit">
-                <v-avatar size="40">
-                  <v-img class="mx-auto" :src="post.creator.avatar" />
-                </v-avatar>
-                <div class="text-h6 ml-1">
-                  {{ post.creator.username }}
-                </div>
-              </router-link>
-            </div>
-            <v-divider vertical v-if="$vuetify.breakpoint.smAndUp"/>
-            <div style="width: 100%">
-              <div>
-                <v-card-text class="d-flex">
-                  {{ utils.formatDate(post.created) }}
-                  <div class="ml-auto">
-                    <v-chip round v-if="post.creator.id !== thread.creator.id"
-                            color="primary" small>
-                      <v-icon small left>
-                        mdi-shield-sword-outline
-                      </v-icon>
-                      <span>
-                        {{ $t('_ticket.staff') }}
-                      </span>
-                    </v-chip>
-                  </div>
-                </v-card-text>
-                <v-divider/>
-                <v-card-text>
-                  <span v-html="post.content" class="ql-editor pa-0"/>
-                </v-card-text>
+    <div v-if="thread">
+      <v-card flat outlined class="vh-forum-post card-rounded mb-3"
+              :class="{ 'card-rounded-top':!$vuetify.breakpoint.smAndDown && index === 0,
+           'no-top-border-radius': $vuetify.breakpoint.smAndDown && index === 0 }"
+              v-for="(post, index) in posts" :key="post.id">
+        <div class="d-flex" :class="{ 'flex-column' : $vuetify.breakpoint.xs }">
+          <div class="pa-3 text-center" style="width: 150px" v-if="$vuetify.breakpoint.smAndUp">
+            <router-link :to="{ name: 'UserDashboard', params: {id: post.creator.id}}"
+                         class="stylelint" style="color: inherit">
+              <v-avatar size="80">
+                <v-img class="mx-auto" :src="post.creator.avatar" />
+              </v-avatar>
+              <div class="text-h6">
+                {{ post.creator.username }}
               </div>
+            </router-link>
+          </div>
+          <div v-else class="pa-3">
+            <router-link :to="{ name: 'UserDashboard', params: {id: post.creator.id}}"
+                         class="stylelint d-flex justify-center align-center"
+                         style="color: inherit">
+              <v-avatar size="40">
+                <v-img class="mx-auto" :src="post.creator.avatar" />
+              </v-avatar>
+              <div class="text-h6 ml-1">
+                {{ post.creator.username }}
+              </div>
+            </router-link>
+          </div>
+          <v-divider vertical v-if="$vuetify.breakpoint.smAndUp"/>
+          <div style="width: 100%">
+            <div>
+              <v-card-text class="d-flex">
+                {{ utils.formatDate(post.created) }}
+                <div class="ml-auto">
+                  <v-chip round v-if="post.creator.id !== thread.creator.id"
+                          color="primary" small>
+                    <v-icon small left>
+                      mdi-shield-sword-outline
+                    </v-icon>
+                    <span>
+                      {{ $t('_ticket.staff') }}
+                    </span>
+                  </v-chip>
+                </div>
+              </v-card-text>
+              <v-divider/>
+              <v-card-text>
+                <span v-html="post.content" class="ql-editor pa-0"/>
+              </v-card-text>
             </div>
           </div>
-        </v-card>
-      </div>
+        </div>
+      </v-card>
     </div>
-    <div>
+    <div v-if="thread" class="d-flex align-center flex-wrap">
+      <v-pagination v-if="totalPages > 1"
+                    v-model="page"
+                    :length="totalPages"
+                    :total-visible="5"
+                    @input="fetchData"/>
+      <v-spacer/>
+      <v-btn v-if="thread.ban" color="primary" :to="{ name: 'Bans',
+       params: {banId: thread.ban.id} }" class="mr-3">
+        <v-icon left>mdi-eye</v-icon>
+        {{ $t('_forum.showBan') }}
+      </v-btn>
+      <v-btn v-if="$checkProp('ticket_edit')"
+             :color="thread.status === 'CLOSED' ? 'success' : 'error'"
+             @click="toggleStatus" depressed>
+        <div v-if="thread.status === 'CLOSED'">
+          <v-icon left>mdi-lock-open-variant</v-icon>
+          <span>{{ $t('_forum.open') }}</span>
+        </div>
+        <div v-else>
+          <v-icon left>mdi-lock</v-icon>
+          <span>{{ $t('_forum.close') }}</span>
+        </div>
+      </v-btn>
+      <v-btn color="success" depressed class="ml-3" @click="$refs.addPostDialog.show()">
+        <v-icon left>mdi-plus</v-icon>
+        <span>{{ $t('_forum.addPost') }}</span>
+      </v-btn>
     </div>
-    <div class="mt-3" v-if="thread">
-      <v-row>
-        <v-col class="hidden-md-and-down" cols="2" lg="1"></v-col>
-        <v-col>
-          <v-pagination v-if="totalPages > 1"
-                        v-model="page"
-                        :length="totalPages"
-                        :total-visible="5"
-                        @input="fetchData"/>
-        </v-col>
-        <v-col cols="12" lg="6" class="d-flex align-center">
-          <v-spacer/>
-          <v-btn v-if="thread.ban" color="primary" :to="{ name: 'Bans',
-           params: {banId: thread.ban.id} }" class="mr-3">
-            <v-icon left>mdi-eye</v-icon>
-            {{ $t('_forum.showBan') }}
-          </v-btn>
-          <v-btn v-if="$checkProp('ticket_edit')"
-                 :color="thread.status === 'CLOSED' ? 'success' : 'error'"
-                 @click="toggleStatus">
-            <div v-if="thread.status === 'CLOSED'">
-              <v-icon left>mdi-lock-open-variant</v-icon>
-              <span>{{ $t('_forum.open') }}</span>
-            </div>
-            <div v-else>
-              <v-icon left>mdi-lock</v-icon>
-              <span>{{ $t('_forum.close') }}</span>
-            </div>
-          </v-btn>
-          <v-btn color="success" class="ml-3" @click="$refs.addPostDialog.show()">
-            <v-icon left>mdi-plus</v-icon>
-            <span>{{ $t('_forum.addPost') }}</span>
-          </v-btn>
-          <v-spacer v-if="$vuetify.breakpoint.mdAndDown"/>
-        </v-col>
-      </v-row>
+    <div v-if="!posts">
+      <v-skeleton-loader type="article" class="card-rounded"/>
+      <v-skeleton-loader class="mt-3 card-rounded" type="article"/>
     </div>
   </div>
 </template>
