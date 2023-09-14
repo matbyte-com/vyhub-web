494a501efbb9fce05201acae2b626a26a5a5870a
Franz <franz0905@web.de>
2023-09-14 14:18:40 +0200

    Fix feature cards on small screens

--- src/components/BuilderComponents/ColumnCards.vue
+++ src/components/BuilderComponents/ColumnCards.vue
@@ -24,23 +24,31 @@
         </v-card>
       </v-col>
     </v-row>
-    <Swiper :number-of-elements="cards.length" :per-page-custom="[1,1,3,3,3]" v-else>
-      <swiper-slide v-for="(card, index) in cards" :key="index">
-        <v-card class="grow-on-hover feature-card flex-grow-1">
-          <v-card-text class="text-center d-flex flex-column" style="height: 100%">
-            <v-icon size="75">{{ card.icon }}</v-icon>
-            <h3 class="text-h5">{{ card.title }}</h3>
-            <p v-html="card.content"></p>
-            <v-spacer />
-            <div>
-              <v-btn outlined color="primary"
-                     :href="(!utils.localLink(card) ? card.link : null)"
-                     :to="utils.localLink(card) ? utils.getLocalLink(card) : null">
-                {{ card.btnText }}
-              </v-btn>
+    <Swiper :number-of-elements="cards.length" :per-page-custom="[1,2,3,3,3]" v-else>
+      <swiper-slide v-for="(card, index) in cards" :key="index" style="height: 130%">
+        <div class="pt-10">
+          <v-card class="grow-on-hover feature-card flex-grow-1 card-rounded text-center"
+                  style="width: inherit">
+            <div class="icon-wrapper">
+              <v-card class="icon-rounded d-inline-block pa-1" flat>
+                <v-icon size="75">{{ card.icon }}</v-icon>
+              </v-card>
             </div>
-          </v-card-text>
-        </v-card>
+            <v-card-text class="text-center d-flex flex-column"
+                         style="height: 100%; width: inherit">
+              <h3 class="text-h5 mt-13">{{ card.title }}</h3>
+              <p v-html="card.content"></p>
+              <v-spacer />
+              <div>
+                <v-btn outlined color="primary"
+                       :href="(!utils.localLink(card) ? card.link : null)"
+                       :to="utils.localLink(card) ? utils.getLocalLink(card) : null">
+                  {{ card.btnText }}
+                </v-btn>
+              </div>
+            </v-card-text>
+          </v-card>
+        </div>
       </swiper-slide>
     </Swiper>
   </div>
