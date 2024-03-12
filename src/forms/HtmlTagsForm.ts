5e114e6f74e4ef28c3758b3f9e0a6f7412a8c491
Jakob Mueller <me@jakobm.de>
2024-03-12 20:54:37 +0100

    Fix html tags form label

--- src/forms/HtmlTagsForm.ts
+++ src/forms/HtmlTagsForm.ts
@@ -26,7 +26,7 @@ export default {
     },
     image_url: {
       type: ['string', 'null'],
-      title: i18n.t('imageUrl'),
+      title: i18n.t('_settings.labels.previewImageURL'),
       pattern: 'https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)',
       'x-props': {
         clearable: true,
