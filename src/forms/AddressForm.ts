89b384ed1f6fa9ac7726ac98dc92e1ead7a29607
Jakob Mueller <me@jakobm.de>
2024-08-03 10:30:38 +0200

    Limit Address length

--- src/forms/AddressForm.ts
+++ src/forms/AddressForm.ts
@@ -10,28 +10,37 @@ export default {
     name: {
       type: 'string',
       title: i18n.t('name'),
+      maxLength: 50,
+      minLength: 2,
     },
     street_and_number: {
       type: 'string',
       title: i18n.t('_address.labels.streetAndNumber'),
+      maxLength: 50,
+      minLength: 2,
     },
     addition: {
       type: 'string',
       title: i18n.t('_address.labels.addition'),
+      maxLength: 40,
     },
     zip_code: {
       type: 'string',
       title: i18n.t('_address.labels.zip_code'),
       'x-cols': 4,
+      maxLength: 10,
     },
     city: {
       type: 'string',
       title: i18n.t('_address.labels.city'),
       'x-cols': 8,
+      maxLength: 40,
+      minLength: 2,
     },
     state: {
       type: 'string',
       title: i18n.t('_address.labels.state'),
+      maxLength: 40,
     },
     country_code: Common.countryCodeField,
     vat_number: {},
