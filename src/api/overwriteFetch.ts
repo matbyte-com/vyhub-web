e1f2aa2cad9cc3ad8eca2b4dfb42a261fc685db7
Jakob Mueller <me@jakobm.de>
2024-10-28 16:08:15 +0100

    Only send Auth header for backend

--- src/api/overwriteFetch.ts
+++ src/api/overwriteFetch.ts
@@ -1,3 +1,5 @@
+import config from '../config';
+
 interface Header {
   name: string;
   value: string;
@@ -9,10 +11,14 @@ const originalFetch = window.fetch;
 window.fetch = async (input: RequestInfo | URL, init?: RequestInit | undefined): Promise<Response> => {
   init = init || {};
 
-  init.headers = {
-    ...fetchHeaders,
-    ...(init.headers as Record<string, string>),
-  };
+  // Only add header if request is for backend url
+  if ((typeof input === 'string' && input.startsWith(config.backend_url)) ||
+    (input instanceof URL && input.href.startsWith(config.backend_url))) {
+    init.headers = {
+      ...fetchHeaders,
+      ...(init.headers as Record<string, string>),
+    };
+  }
 
   return originalFetch(input, init);
 };
