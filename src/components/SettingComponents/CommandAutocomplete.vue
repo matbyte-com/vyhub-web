99acbdb61685a45b3acf9b899c1c594d84206f49
Franz Rodestock <franz0905@web.de>
2024-11-12 18:47:12 +0100

    Change command delimiters to % instead of {}

--- src/components/SettingComponents/CommandAutocomplete.vue
+++ src/components/SettingComponents/CommandAutocomplete.vue
@@ -2,7 +2,6 @@
   <div>
     <!-- TODO Label Not Working -->
     <span class="text-subtitle-1">{{ label }}</span>
-    {{ formModel }}
     <v-combobox
       ref="autocomplete"
       :model-value="command"
@@ -37,8 +36,14 @@ const props = defineProps({
 
 const autocomplete = ref(null);
 const command = ref('');
-// TODO ADD ALL REPLACEMENTS
-const options = {MINECRAFT: ['{username}', '{map}'], GMOD: ['{player}', '{map}']};
+const options = {
+  ASA: ['%user_id%', '%nick%', '%player_id%', '%applied_packet_id%', '%packet_title%', '%purchase_amount%'],
+  FIVEM: ['%id%', '%user_id%', '%char_name%', '%nick%', '%license%', '%steam_id%', '%applied_packet_id%', '%packet_title%', '%purchase_amount%'],
+  MINECRAFT: ['%user_id%', '%nick%', '%player_id%', '%player_ip_address%', '%applied_packet_id%', '%packet_title%', '%purchase_amount%'],
+  GMOD: ['%user_id%', '%nick%', '%steamid64%', '%steamid32%', '%uniqueid%', '%applied_packet_id%', '%packet_title%', '%purchase_amount%'],
+  RUST: ['%user_id%', '%nick%', '%applied_packet_id%', '%steamid64%', '%packet_title%', '%purchase_amount%'],
+  SEVEN_DAYS: ['%user_id%', '%nick%', '%applied_packet_id%', '%steamid64%', '%packet_title%', '%purchase_amount%']
+};
 const autocompleteOpen = ref(false);
 
 const computedOptions = computed(() => {
@@ -56,13 +61,13 @@ const getCurrentTemplateBounds = () => {
 
   // Find the opening brace before cursor
   const textBeforeCursor = fullText.slice(0, cursorPos);
-  const lastOpenBraceIndex = textBeforeCursor.lastIndexOf('{');
+  const lastOpenBraceIndex = textBeforeCursor.lastIndexOf('%');
 
   if (lastOpenBraceIndex === -1) return null;
 
   // Find the closing brace after the opening brace
   const textAfterOpen = fullText.slice(lastOpenBraceIndex);
-  const nextCloseBraceIndex = textAfterOpen.indexOf('}');
+  const nextCloseBraceIndex = textAfterOpen.indexOf('%');
 
   // If we found both braces, return the bounds
   if (nextCloseBraceIndex !== -1) {
@@ -87,7 +92,7 @@ const filterOptions = (item, queryText, itemText) => {
   const input = autocomplete.value.$el.querySelector('input');
   const cursorPos = input.selectionStart;
   const textBeforeCursor = input.value.slice(0, cursorPos);
-  const lastOpenBraceIndex = textBeforeCursor.lastIndexOf('{');
+  const lastOpenBraceIndex = textBeforeCursor.lastIndexOf('%');
 
   if (lastOpenBraceIndex !== -1) {
     const query = textBeforeCursor.slice(lastOpenBraceIndex + 1).toLowerCase();
@@ -117,7 +122,7 @@ const onAutocompleteChange = (newValue) => {
   }
 
   // Remove the braces from the selected option
-  const selectedValue = newValue.replace(/[{}]/g, '');
+  const selectedValue = newValue.replace(/[%]/g, '');
 
   // Get the current input value (not the v-model value)
   const currentInputValue = input.value;
@@ -125,7 +130,7 @@ const onAutocompleteChange = (newValue) => {
   // Reconstruct the command with the new template variable
   const newCommand =
     currentInputValue.slice(0, bounds.start) +
-    '{' + selectedValue + '}' +
+    '%' + selectedValue + '%' +
     currentInputValue.slice(bounds.end);
 
   // Update the command
