2035e71eda8766ba30fbe47602cf8f92dd77a187
Jakob Mueller <me@jakobm.de>
2026-02-14 18:39:39 +0100

    Fix features margin

--- src/components/BuilderComponents/BuilderColumnCards.vue
+++ src/components/BuilderComponents/BuilderColumnCards.vue
@@ -1,65 +1,19 @@
 <template>
-  <div class="vh-home-features">
-    <v-row
-      v-if="$vuetify.display.mdAndUp"
-      justify="center"
-    >
-      <v-col
-        v-for="(card, index) in cards"
-        :key="index"
-        cols="6"
-        md="4"
-        lg="3"
-        class="d-flex flex-column"
+  <div>
+    <div class="vh-home-features mt-4">
+      <v-row
+        v-if="$vuetify.display.mdAndUp"
+        justify="center"
       >
-        <v-card class="grow-on-hover feature-card flex-grow-1 card-rounded text-center mt-8">
-          <div class="icon-wrapper">
-            <v-card
-              class="icon-rounded d-inline-block pa-1 v-card-non-transparent"
-              flat
-            >
-              <v-icon size="75">
-                {{ card.icon }}
-              </v-icon>
-            </v-card>
-          </div>
-          <v-card-text
-            class="text-center d-flex flex-column"
-            style="height: 100%"
-          >
-            <h3 class="text-h5 mt-13">
-              {{ card.title }}
-            </h3>
-            <p v-html="card.content" />
-            <v-spacer />
-            <div>
-              <v-btn
-                variant="flat"
-                color="primary"
-                :href="(!utils.localLink(card) ? card.link : null)"
-                :to="utils.localLink(card) ? utils.getLocalLink(card) : null"
-              >
-                {{ card.btnText }}
-              </v-btn>
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-    <Swiper
-      v-else
-      :number-of-elements="cards.length"
-      :per-page-custom="[1,2,3,3,3,3]"
-    >
-      <swiper-slide
-        v-for="(card, index) in cards"
-        :key="index"
-      >
-        <div class="pt-10">
-          <v-card
-            class="grow-on-hover feature-card flex-grow-1 card-rounded text-center"
-            style="width: inherit"
-          >
+        <v-col
+          v-for="(card, index) in cards"
+          :key="index"
+          cols="6"
+          md="4"
+          lg="3"
+          class="d-flex flex-column"
+        >
+          <v-card class="grow-on-hover feature-card flex-grow-1 card-rounded text-center mt-8">
             <div class="icon-wrapper">
               <v-card
                 class="icon-rounded d-inline-block pa-1 v-card-non-transparent"
@@ -72,7 +26,7 @@
             </div>
             <v-card-text
               class="text-center d-flex flex-column"
-              style="height: 100%; width: inherit"
+              style="height: 100%"
             >
               <h3 class="text-h5 mt-13">
                 {{ card.title }}
@@ -91,9 +45,57 @@
               </div>
             </v-card-text>
           </v-card>
-        </div>
-      </swiper-slide>
-    </Swiper>
+        </v-col>
+      </v-row>
+      <Swiper
+        v-else
+        :number-of-elements="cards.length"
+        :per-page-custom="[1,2,3,3,3,3]"
+      >
+        <swiper-slide
+          v-for="(card, index) in cards"
+          :key="index"
+        >
+          <div class="pt-10">
+            <v-card
+              class="grow-on-hover feature-card flex-grow-1 card-rounded text-center"
+              style="width: inherit"
+            >
+              <div class="icon-wrapper">
+                <v-card
+                  class="icon-rounded d-inline-block pa-1 v-card-non-transparent"
+                  flat
+                >
+                  <v-icon size="75">
+                    {{ card.icon }}
+                  </v-icon>
+                </v-card>
+              </div>
+              <v-card-text
+                class="text-center d-flex flex-column"
+                style="height: 100%; width: inherit"
+              >
+                <h3 class="text-h5 mt-13">
+                  {{ card.title }}
+                </h3>
+                <p v-html="card.content" />
+                <v-spacer />
+                <div>
+                  <v-btn
+                    variant="flat"
+                    color="primary"
+                    :href="(!utils.localLink(card) ? card.link : null)"
+                    :to="utils.localLink(card) ? utils.getLocalLink(card) : null"
+                  >
+                    {{ card.btnText }}
+                  </v-btn>
+                </div>
+              </v-card-text>
+            </v-card>
+          </div>
+        </swiper-slide>
+      </Swiper>
+    </div>
   </div>
 </template>
 
