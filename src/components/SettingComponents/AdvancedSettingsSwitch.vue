71a07961298c19199fa0ec028756288805b19eed
Jakob Mueller <me@jakobm.de>
2026-08-02 13:04:43 +0200

    Reload page when enabling/disabling advanced settings

--- src/components/SettingComponents/AdvancedSettingsSwitch.vue
+++ src/components/SettingComponents/AdvancedSettingsSwitch.vue
@@ -20,7 +20,6 @@
 
 <script>
 import openapi from '@/api/openapi';
-import EventBus from '@/services/EventBus';
 
 export default {
   data() {
@@ -38,14 +37,9 @@ export default {
   methods: {
     async toggleAdvancedSettings(val) {
       (await openapi).general_editConfig(null, { show_advanced_settings: val }).then(() => {
-        // caught in App.vue to Update Theme + Header
-        EventBus.emit('themeUpdated');
-        // caught in Settings.vue to rerender the Forms
-        EventBus.emit('advancedSettingsUpdated');
-        this.$notify({
-          title: this.$t('_messages.editSuccess'),
-          type: 'success',
-        });
+        // Forms (e.g. ServerForm) read show_advanced_settings at build time, so a
+        // full reload is needed for the change to take effect everywhere.
+        window.location.reload();
       }).catch(() => {
         this.model = !val;
       });
